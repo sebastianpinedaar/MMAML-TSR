ffa2a4198d1bb955cc1529775934d8c62dacf68f
releaunifreiburg <relea@informatik.uni-freiburg.de>
2022-04-27 19:24:49 +0200

    utils module

---
+++ code/pre_processing/utils.py
@@ -0,0 +1,7 @@
+
+def progressBar(current, total, barLength = 20):
+    percent = float(current) * 100 / total
+    arrow   = '-' * int(percent/100 * barLength - 1) + '>'
+    spaces  = ' ' * (barLength - len(arrow))
+
+    print('Progress: [%s%s] %d %%' % (arrow, spaces, percent), end='\r')
